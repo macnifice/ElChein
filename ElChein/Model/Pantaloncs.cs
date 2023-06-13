1c8c233e18c9ce985085a085996b156ff9f77a36
luis_ <luis_@LAPTOP-M6NFFRIG>
2023-06-12 20:15:11 -0600

    Add project files.

---
+++ ElChein/Model/Pantaloncs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElChein.Model
+{
+    public class Pantaloncs
+    {
+        [Key]
+        int IdPants { get; set; }
+        string Size { get; set; }
+
+        string Color { get; set; }
+
+        string Material { get; set; }
+
+        double Price { get; set; }
+    }
+}
---
+++ ElChein/Model/Playera.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElChein.Model
+{
+    public class Playera
+    {
+        [Key]    
+        int IdShirt { get; set; }
+        string Size { get; set; }
+
+        string Color { get; set; }
+
+        string Material { get; set; }
+
+        double Price { get; set; }
+
+    }
+}
