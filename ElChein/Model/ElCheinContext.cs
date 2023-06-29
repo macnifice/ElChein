ee50072a2540cab7cb83840dcf93cb0e461d49b2
Luis Chavez <luis99ch@gmail.com>
2023-06-28 22:30:30 -0600

    se creo branch arcane, configuración del ordenador

--- ElChein/Model/ElCheinContext.cs
+++ ElChein/Model/ElCheinContext.cs
@@ -21,7 +21,7 @@ public partial class ElCheinContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-8CVQ4M4\\SQLEXPRESS;Initial Catalog=ElChein;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-A30C9EP\\SQLEXPRESS;Initial Catalog=ElChein;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
